e160e104fa6f36a05570bc115a6d6dc75856972d
carlos0718 <c.sipiran07@gmail.com>
2025-09-02 20:47:29 -0300

    nueva config de git

---
+++ Models/ObraSocial.cs
@@ -0,0 +1,22 @@
+﻿namespace WebAppSistemaMedico.Models
+{
+    /*
+     Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+        Nombre NVARCHAR(100) NOT NULL UNIQUE,
+        Codigo NVARCHAR(10) NOT NULL UNIQUE,
+        Descripcion NVARCHAR(255) NULL,
+        Activo BIT NOT NULL DEFAULT 1,
+        FechaCreacion DATETIME2(7) NOT NULL DEFAULT GETDATE(),
+        FechaModificacion DATETIME2(7) NULL
+     */
+    public class ObraSocial
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public bool Activo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+    }
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,28 @@
+﻿namespace WebAppSistemaMedico.Models
+{
+
+    /*
+        Id int primary key identity(1,1) not null,
+        Nombre varchar(15) not null,
+        Apellido varchar(30) not null,
+        FechaNacimiento datetime not null,
+        Genero varchar(15) not null,
+        DNI varchar(12) unique not null,
+        Domicilio varchar(150),
+        Telefono varchar(20),
+        Email varchar(100),
+     */
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public string? Genero { get; set; }
+        public string? DNI { get; set; }
+        public string? Domicilio { get; set; }
+        public string? Telefono { get; set; }
+        public string? Email { get; set; }
+
+    }
+}
