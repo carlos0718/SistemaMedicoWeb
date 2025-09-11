f67264b78b5ecfdae12345d064e7182f9540ae1f
martinangelrojas <rojasverdaderobis@gmail.com>
2025-09-11 19:55:03 -0300

    Usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace WebAppSistemaMedico.Models
+{
+    public class Usuario
+    {
+
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public int PersonaId { get; set; }
+        public int TipoUsuario { get; set; }
+        public bool? Activo { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+    }
+}
